5fea41921794e3ff3e956e86e595f43cf579ce9d
Math Carvalho <mcarvalho@localhost.localdomain>
2016-11-06 21:36:03 -0200

    adding main file skelet

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python
 
+import string
 from include import Votacao
 from Tkinter import *
 
 '''
 Projeto De Modelagem de Sistemas
 '''
+gui = Tk()
+
+Cargo = "~Presidente~"
+ment = StringVar()
+
+def callback():
+    mtxt = ment.get()
+    labelA = Label(text = mtxt).grid(row= 6, column = 6)
+
+gui.geometry('600x600+500+500')
+gui.title("Uev Rio de Janeiro")
+
+xEntry = Entry(gui,textvariable = ment).grid(row= 1, column = 1)
+labelA = Label(text = Cargo).grid(row= 0, column = 1)   
+
+B1 = Button(gui, text= "Ver", command = callback).grid(row = 3, column = 2)
+B2 = Button(gui, text= "Confirma", command = callback).grid(row= 3, column = 2)
+B2 = Button(gui, text= "Nulo", command = callback).grid(row= 2, column = 0)
+B2 = Button(gui, text= "Branco", command = callback).grid(row= 2, column = 1)
+
+gui.mainloop()
